09278142633d7de56580f17db6a610c95e642513
John Riney III <john@rineysoft.com>
2023-03-11 18:03:37 -0800

    Proper logging

--- src/realc64bot/config.py
+++ src/realc64bot/config.py
@@ -1,4 +1,7 @@
-import json, os
+import json, logging, os
+
+logger = logging.getLogger('config')
+logging.basicConfig(level=logging.INFO)
 
 class Config(object):
     _instance = None
@@ -9,11 +12,11 @@ class Config(object):
 
     def __new__(cls):
         if cls._instance is None:
-            print('Creating config.')
+            logger.info('Creating config.')
             cls._instance = super(Config, cls).__new__(cls)
             config_file_name = os.environ.get(Config.CONFIG_FILE_NAME_ENV_VAR) or Config.DEFAULT_CONFIG_FILE_NAME
 
-            print(f"Loading config from {config_file_name}")
+            logger.info(f"Loading config from {config_file_name}")
             with open(config_file_name) as f:
                 cls._instance.config = json.load(f)
 
--- src/realc64bot/gateways/mastodon_source.py
+++ src/realc64bot/gateways/mastodon_source.py
@@ -8,22 +8,25 @@ from realc64bot.connectors.work_queue import WorkQueue
 mastodon = None
 channel = None
 
+logger = logging.getLogger('mastodon_source')
+logging.basicConfig(level=logging.INFO)
+
 def handle_notification(notification):
-    print("mastodon_source: handle_notification: got an notification!")
+    logger.info("handle_notification: got an notification!")
     type = notification['type']
     if type == 'mention':
         id = notification['status']['id']
         content = notification['status']['content']
         toot = strip_toot(content)
 
-        print(f'mastodon_source: handle_notification: got status {id} with type "{type}" and toot "{toot}"')
+        logger.info(f'mastodon_source: handle_notification: got status {id} with type "{type}" and toot "{toot}"')
         message = {
             "message_id": id,
             "message_source": "mastodon",
             "content": toot,
         }
 
-        print(f"mastodon_source: handle_notification: enqueuing message {message}")
+        logger.info(f"handle_notification: enqueuing message {message}")
         loop.create_task(enqueue(message))
 
 def strip_toot(content):
@@ -33,7 +36,7 @@ def strip_toot(content):
     return navstrs[0].strip() or ""
 
 async def enqueue(message) -> None:
-    print("mastodon_source: publishing message")
+    logger.info("publishing message")
     await channel.default_exchange.publish(
         aio_pika.Message(body=bytes(json.dumps(message), encoding='utf8')),
         routing_key=WorkQueue.MESSAGES_QUEUE,
@@ -43,23 +46,24 @@ async def main() -> None:
     config = Config().values()
 
     try:
-        print("mastodon_source: connecting to work queue")
+        logger.info("connecting to work queue")
         connection = await aio_pika.connect_robust(f"amqp://{config['rabbitmq']['username']}:{config['rabbitmq']['password']}@{config['rabbitmq']['host']}")
         global channel
         channel = await connection.channel()
+        await channel.declare_queue(WorkQueue.MESSAGES_QUEUE)        
         
-        print("mastodon_source: connecting to mastodon")
+        logger.info("connecting to mastodon")
         global mastodon
         mastodon = Mastodon(
             client_id=config['mastodon']['client_id'],
             access_token=config['mastodon']['access_token'])
        
-        print("mastodon_source: setting version")
+        logger.info("setting version")
         v = mastodon.retrieve_mastodon_version()
-        print(f"mastodon_source: got version #{v}")
+        logger.info(f"got version #{v}")
         listener = CallbackStreamListener(notification_handler = handle_notification)
 
-        print("mastodon_source: starting notification stream...")
+        logger.info("starting notification stream...")
         global stream
         stream = mastodon.stream_user(listener,
             run_async = True,
@@ -69,21 +73,21 @@ async def main() -> None:
         global loop
         loop = asyncio.get_running_loop()
 
-        print("mastodon_source: awaiting the future...")
+        logger.info("awaiting the future...")
         await asyncio.Future()
         
     except Exception:
-        print("mastodon_source: exception")
+        logger.info("exception")
 
-        traceback.print_exc(file=sys.stdout)
+        traceback.logger.info_exc(file=sys.stdout)
     sys.exit(0)
 
 if __name__ == "__main__":
-    print("mastodon_source: in startup")
+    logger.info("in startup")
     try:
         asyncio.run(main())
 
     except KeyboardInterrupt:
-        print("mastodon_source: Shutdown requested... closing Mastodon stream")
+        logger.info("Shutdown requested... closing Mastodon stream")
         stream.close()
-        print("mastodon_source: exiting")
+        logger.info("exiting")
