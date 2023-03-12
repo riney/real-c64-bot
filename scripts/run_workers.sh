7c38bc83b89d07242117c081c6810e3f5c3669b2
John Riney III <john@rineysoft.com>
2023-03-11 18:27:32 -0800

    cleanup, added supervisord

--- scripts/run_workers.sh
+++ scripts/run_workers.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 
+PYTHONPATH=src python src/realc64bot/gateways/message_worker.py &
+#PYTHONPATH=src python src/realc64bot/gateways/tokenize_worker.py &
+#PYTHONPATH=src python src/realc64bot/gateways/reply_worker.py &
